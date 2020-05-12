b79f44ae92b82bde96c0fb015a5f698e101af8a5
Alyona Bugai <59482685+AlyonaBugai@users.noreply.github.com>
2020-05-12 18:37:22 +0300

    Initial commit

---
+++ calculatorapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+# from calculatorapp.logic.logic import SymPyGamma
+import wolframalpha
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def input(request):
+    if request.method == "GET":
+        q = request.GET['query']
+
+        # g = SymPyGamma()
+        # r = g.eval(q)
+        # r = eval(q)
+        client = wolframalpha.Client('PL5HW2-Q2XGXA6RAV')
+        res = client.query(q)
+        if not res:
+            mydictionary = [{
+                "title": "Input",
+                "input": input,
+                "output": "Can't handle the input."
+            }]
+        else:
+            mydictionary = {
+                "q": q,
+                "ans": next(res.results).text,
+                "pic": res['pod'][1]['subpod']['img']['@src'],
+                "error": False,
+                "result": True
+            }
+
+        return render(request, 'index.html', context=mydictionary)
